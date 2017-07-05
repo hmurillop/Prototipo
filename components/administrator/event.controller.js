ccdd2871d34384a451b4a24a5e17cf593dbbb618
keilynorozcoc <korozcoc@ucenfotec.ac.cr>
2017-07-05 11:38:19 -0600

    img en proceso

--- components/administrator/event.controller.js
+++ components/administrator/event.controller.js
@@ -19,8 +19,7 @@
         Upload.upload(vm.cloudObj)
           .success(function(data){
             pNewEvent.photo = data.url;
-            vm.save(data.url);
-
+            vm.save(pNewEvent);
           });
       }
 
@@ -51,6 +50,7 @@
         vm.event.business = pEvent.business;
         vm.event.typeSponsor = pEvent.typeSponsor;
         vm.event.brand = pEvent.brand;
+        vm.event.photo = pEvent.photo;
       }
 
       vm.update = function(){
@@ -73,7 +73,7 @@
           brand: vm.event.brand,
           business: vm.event.business,
           typeSponsor: vm.event.typeSponsor,
-          selectFile: vm.event.selectFile
+          photo: vm.event.photo
         }
         eventService.updateEvent(eventEdited);
         clean();
